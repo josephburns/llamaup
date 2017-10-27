5ec7f0a12a6d0ed81858b3c405b2ec4e2e9dd224
Joseph Burns <jburns@projectscribe.com>
2017-10-27 16:56:55 -0400

    add companies alphabetically endpoint

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,8 @@ class CompaniesController < ApplicationController
   def index
     render json: { data: Company.all }
   end
+
+  def alphabetically
+    render json: { data: Company.order(:name)}
+  end
 end
--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -17,4 +17,27 @@ RSpec.describe CompaniesController, type: :controller do
     end
   end
 
+  describe "get #alphabetically" do
+    it "returns http success" do
+      get :alphabetically
+      expect(response).to have_http_status(:success)
+    end
+    it "returns companies in alphabetical order" do
+      semi_rando_array = []
+      10.times do
+        semi_rando_name = ('a'..'z').to_a.shuffle[0,10].join
+        # pull it out into an array to compare later
+        semi_rando_array << semi_rando_name
+        create(:company, :name => semi_rando_name, :plan_level => 'custom')
+      end
+      get :alphabetically
+      json = JSON.parse(response.body)
+      response_names = []
+      for i in 0..9
+        response_names << json['data'][i]['name']
+      end
+      expect(response_names).to eq(semi_rando_array.sort)
+    end
+  end
+
 end
